189ac8f28301f40a96be591fb536c124e9924e62
Kanji-Ishii <ec.2kng@gmail.com>
2021-10-06 02:01:40 +0900

    create: migration file CreateExercise, CreateRecord

---
+++ db/migrate/20211005165602_create_exercise.rb
@@ -0,0 +1,10 @@
+class CreateExercise < ActiveRecord::Migration[6.1]
+  def change
+    create_table :exercises do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :name, null: false
+
+      t.timestamps
+    end
+  end
+end
