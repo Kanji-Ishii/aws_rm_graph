80982da3765021730a9f8a788acafc241ae6da64
Kanji-Ishii <ec.2kng@gmail.com>
2021-10-06 02:10:55 +0900

    update: RM column change decimal at Records table

--- db/migrate/20211005170534_create_exercises.rb
+++ db/migrate/20211005170534_create_exercises.rb
@@ -1,6 +1,8 @@
 class CreateExercises < ActiveRecord::Migration[6.1]
   def change
     create_table :exercises do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :name, null: false
 
       t.timestamps
     end
