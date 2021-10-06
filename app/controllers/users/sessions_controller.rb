57bad079ce687e49227cffaf0aab56cb03cd0047
Kanji-Ishii <ec.2kng@gmail.com>
2021-10-06 11:57:44 +0900

    Exec: yarn add chartkick chart.js to desplayed chart

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -20,7 +20,7 @@ class Users::SessionsController < Devise::SessionsController
 
   protected
 
-  def after_sign_up_path_for(resource)
+  def after_sign_in_path_for(resource)
     exercises_path
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(name: 'sample', email: 'sample@sample.com', password: 'foobar')
+
+User.all.each do |user|
+  user.exercises.create!(id: 0, name: 'ダンベル プレス')
+  user.exercises.create!(id: 1, name: 'ダンベル フライ')
+  user.exercises.create!(id: 2, name: 'アーム カール')
+  user.exercises.create!(id: 3, name: 'トライセプス エクステンション')
+end
+
+require "csv"
+CSV.foreach('db/seed_record.csv') do |row|
+  Record.create(exercise_id: row[0],
+                    name_id: row[1],
+                     weight: row[2],
+                 reps_count: row[3],
+                       note: row[4],
+                         RM: row[5],
+                       date: row[6]
+                )
+end
